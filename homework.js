fe8e97e2f93e2022a14099f254eea94342da3fdb
ASMAA-86 <asmabaabdullah@hotmail.com>
2019-01-20 11:42:55 +0300

    hw mta

---
+++ homework.js
@@ -0,0 +1,16 @@
+var sixthLine = ['Grand Central', '33rd', '28th', '23rd', 'Union Square', 'Astor Place'];
+function planTrip (stopOff)
+{
+for (var i = 0; i< sixthLine.length;i++)
+{
+if (sixthLine[i] !== stopOff){
+console.log(sixthLine[i])
+}
+else {
+
+    console.log("sorry!")  
+}
+}
+
+
+}
