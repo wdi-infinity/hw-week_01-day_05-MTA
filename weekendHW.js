64bcfb9ad8158c17d824d48ae0e4003fcc31815b
ANFAL <anfaljaferi@gmail.com>
2019-01-21 09:22:01 +0300

    MTA HW

---
+++ weekendHW.js
@@ -0,0 +1,8 @@
+//MTA Puzzle
+//planTrip("N", "Times Square", "6", "33rd"); // This is only a suggested function name and signature.
+
+// console.log() shows output similar to this:
+// "You must travel through the following stops on the N line: 34th, 28th, 23rd, Union Square."
+// "Change at Union Square."
+// "Your journey continues through the following stops: 23rd, 28th, 33rd."
+// "7 stops in total."
